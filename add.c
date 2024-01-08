b30480af6c83e3cdf8e0720e1ff81e69891a3551
Jelina-bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-01-08 12:02:27 +0545

    Add files via upload

---
+++ add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    system("cls");
+    int a, b, sum;
+    printf(" enter a number a");
+    scanf("%d", &a);
+    printf("enter a number b");
+    scanf("%d", &b);
+    sum = (a + b);
+    printf("the value of sum is=%d", sum);
+}
