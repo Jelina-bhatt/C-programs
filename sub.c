3046d709539db28ff802409d5b65b8d3fd42ce3d
Jelina-bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-01-08 12:03:01 +0545

    Add files via upload

---
+++ sub.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    system("cls");
+    int a, b, sub;
+    printf("enter a number a");
+    scanf("%d", &a);
+    printf("enter a number b");
+    scanf(" %d", &b);
+    sub = a - b;
+    printf(" the value of sub is= %d",sub);
+    return 0;
+}
