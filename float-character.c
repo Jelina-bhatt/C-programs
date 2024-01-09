2d6553ea1164b3340dda5d8676dd143fd2116abf
Jelina-bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-01-10 00:15:30 +0545

    Add files via upload

---
+++ float-character.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    system("cls");
+    for (float i = 1.0; i <= 5.0; i++)
+    {
+        printf("%f\n", i);
+    }
+    for (char ch = 'a'; ch <= 'z'; ch++)
+    {
+        printf("%c\n", ch);
+    }
+}
