b5490568bdffcaee8f0c846661018a34b85e200d
Jelina-bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-01-09 22:35:02 +0545

    Add files via upload

---
+++ format.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    system("cls");
+    int a, b;
+    printf("enter a integer number");
+    scanf("%d", &a);
+    printf("the stored value of a=%d", a);
+    printf("\n enter another integer number");
+    scanf("%3d", &b);
+    printf("the stored value of b=%d", b);
+}
