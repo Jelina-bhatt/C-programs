18a619a2d25b260b467a3b779938a93bcc07f8ca
Jelina-bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-01-09 22:34:26 +0545

    Add files via upload

---
+++ code.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    system("cls");
+    int c,d;
+    printf("enter a integer number c");
+    scanf("%2d", &c);
+    printf("enter a integer number d");
+    scanf("%6d", &d);
+    printf("\n the value of c :%d",c);
+    printf("\n the value of d:%d",d);
+    getch();
+}
