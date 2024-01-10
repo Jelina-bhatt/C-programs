d44f6c408280cf387b6b134a998c2b002a0c120c
Jelina-bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-01-10 09:13:50 +0545

    Add files via upload

---
+++ jaishreehari.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    system("cls");
+    int n;
+    printf("enter a num:");
+    scanf("%d",&n);
+    int i=0;
+    while(i<=n)
+    {
+        printf("jai shree hari\n");
+        i++;
+    }
+    
+}
