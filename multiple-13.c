370313dc49aae3f0dfc021eb035bd1a80bbb6a8a
Jelina-bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-01-12 10:32:37 +0545

    Add files via upload

---
+++ multiple-13.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    system ("cls");
+    int n;
+    do {
+        printf("enter a number :");
+        scanf("%d",&n);
+        printf("%d\n", n);
+        if ( n%13==0){
+            break;
+        }
+    }while (1);
+    {
+        printf("radheshyam\n");
+    }
+    
+}
