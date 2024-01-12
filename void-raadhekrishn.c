b59bb76a2ff7af70640a1e8adb4db6db1945065a
Jelina-bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-01-12 16:18:20 +0545

    Add files via upload

---
+++ void-raadhekrishn.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void printraadhe();
+void printkrishn();
+
+int main(){
+    printraadhe();
+    printkrishn();
+    return 0;
+}
+
+void printraadhe(){
+    printf("raadhe\n");
+}
+void printkrishn(){
+    printf("krishn\n");
+}
