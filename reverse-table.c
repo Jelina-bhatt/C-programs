f544a4735cfe91c19e1ac35cd395c51727eaa859
Jelina-bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-01-12 15:29:51 +0545

    Add files via upload

--- reverse-table.c
+++ reverse-table.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-int main()
-{
+#include<stdio.h>
+int main(){
     system("cls");
-    int n;
-    printf("enter a number :");
-    scanf("%d\n", &n);
-    for (int i = 10; i <= 0; i--)
-    {
-        printf("%d\n", i * n);
+   int n;
+   printf("enter a number :");
+   scanf("%d", &n);
+    for (int i = 10; i >=1; i--){
+        printf("%d\n",i*n);
     }
     return 0;
-}
+    }
