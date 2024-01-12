3725e434a4660bbe5c25a0d26ff831c19a3793a4
Jelina-bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-01-12 12:08:10 +0545

    Add files via upload

---
+++ continue--while.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    system("cls");
+    int i = 1;
+    while (i <= 5)
+    {
+if (i==3)
+{
+    i++;
+  continue;
+
+}
+
+      
+        printf("%d\n", i);
+        i++;
+    
+    }
+}
