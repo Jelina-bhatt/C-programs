27fa38738e6a027b2d96d76b99bb5fbf9d7efb3d
Jelina-bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-01-14 19:11:36 +0545

    Add files via upload

---
+++ greeting.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void printnamaste();
+void printbonjour();
+int main()
+{
+    printnamaste();
+     printbonjour();
+    return 0;
+}
+void printnamaste()
+{
+    printf("namaste\n");
+    
+}
+void printbonjour()
+{
+    printf("bonjour\n");
+}
