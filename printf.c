964df79a16bdce4f0fab8a7ed46c5dd6e1053d31
Jelina Bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-02-15 19:44:35 +0545

    Add files via upload

---
+++ printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    system("cls");
+    int a = 3;
+    float pi = 3.14;
+    char good = 'd';
+    printf("the value of a=%d", a);
+    printf("the value of pi=%f", pi);
+    printf("the char is %c", good);
+    return 0;
+}
