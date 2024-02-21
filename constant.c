f265912df483f2e2961957e265e72c7ae18b17e2
Jelina Bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-02-21 22:45:41 +0545

    Add files via upload

---
+++ constant.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    a=10;
+    b=010;
+    c=0xFf;
+    printf("decimal integer is %d\n", a);
+    printf("octal integer equivalent in decimal is %d\n", b);
+     printf("hexadecimal integer equivalent in decimal is %d\n", c);
+   return 0;
+}
