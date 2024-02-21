e7fc561c3afa3c0611ba3943d51130e3c3791d7b
Jelina Bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-02-21 22:45:20 +0545

    Add files via upload

---
+++ ascii_value.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    char c, d;
+    c = 'A';
+    d = 'E';
+    printf("the character is %c\n", c);
+    printf("the character is %c\n", d);
+    printf("the ASCII value of character is %d\n", c);
+    printf("the ASCII value of character is %d\n", d);
+    return 0;
+}
