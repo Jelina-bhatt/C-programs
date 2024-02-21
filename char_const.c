6d3a56cad0b6d9e5e24147924fc54c7a8fbe2a9e
Jelina Bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-02-21 23:01:43 +0545

    Add files via upload

---
+++ char_const.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int n = 4;
+    char c = '4';
+    n = n + 5;
+    c = c + 5;
+    printf("integer =%d", n);
+    printf("\n character =%d", c);
+    return 0;
+}
