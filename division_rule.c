0453877c4929245422d42bacbbce38fcce4f64c5
Jelina Bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-02-24 21:50:02 +0545

    Add files via upload

---
+++ division_rule.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    float d,e,f;
+    a=10;
+    b=3;
+    c=5;
+    d=20.5;
+    e=10.25;
+    f=3.5;
+    printf("a/b=%d\t a/c=%d\n",a/b,a/c);
+    printf("d/e=%f\t d/f=%f\t d/a=%f",d/e, d/f,d/a);
+    return 0;
+
+
+}
