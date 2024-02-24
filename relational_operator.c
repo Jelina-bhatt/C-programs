87f1558347a1a99c765feec86b1b7e66468dcf35
Jelina Bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-02-24 22:41:23 +0545

    Add files via upload

---
+++ relational_operator.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int a=14,b=33,c=14;
+    printf("a<b=>%d/t a>b=>%d",a<b,a>b);
+    printf("\n a==c => %d\t a<=c=> %d\t a>=c=>%d",a==c, a<=c,a>=c);
+    return 0;
+}
