d69c91726007733d4b9f61ab19dc63aa00f84765
Jelina Bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-03-27 23:12:12 +0545

    Add files via upload

---
+++ if-else.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("enter a non zero num");
+    scanf("%d",&a);
+    if (a%2==0)
+    {
+       printf("%dis even num",a);
+    }
+else
+{
+    printf("%d is odd num",b);
+
+}
+
+ 
+}
