7e6688087936441aee6135b61d7b7d8c01e3b338
Jelina Bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-06-27 21:17:21 +0545

    Create Sum.c

---
+++ array.c/Sum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int numbers[5], sum = 0;
+    float average;
+
+    // Input array elements
+    printf("Enter 5 numbers: \n");
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &numbers[i]);
+        sum += numbers[i];
+    }
+
+    // Calculate average
+    average = sum / 5.0;
+
+    // Output sum and average
+    printf("Sum: %d\n", sum);
+    printf("Average: %.2f\n", average);
+
+    return 0;
+}
