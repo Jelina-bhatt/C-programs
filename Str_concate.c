a5b240989a71bbb3fba4ea2a8f39845ebfa7b53b
Jelina Bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-07-12 22:26:39 +0545

    Create Str_concate.c

---
+++ Str_concate.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[50] = "Hello";
+    char str2[] = ", World!";
+    strcat(str1, str2);
+    printf("Concatenated string: %s\n", str1);
+    return 0;
+}
